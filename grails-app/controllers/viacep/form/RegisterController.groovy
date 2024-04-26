86890362902992aa3e80b3b1e385897afc981bae
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2024-04-26 11:08:06 -0300

    Adiciona controller para lidar com registro

---
+++ grails-app/controllers/viacep/form/RegisterController.groovy
@@ -0,0 +1,21 @@
+package viacep.form
+
+class RegisterController {
+
+    def store() {
+        // Imprime as informações no console
+        println("Nome: " + params["name"])
+        println("CPF: " + params["cpf"])
+        println("Telefone: " + params["phone"])
+        println("CEP: " + params["cep"])
+        println("Rua: " + params["address-str"])
+        println("Número: " + params["address-num"])
+        println("Complemento: " + params["address-adt"])
+        println("Bairro: " + params["district"])
+        println("Cidade: " + params["city"])
+        println("UF: " + params["state"])
+
+        redirect(uri: "/")
+    }
+
+}
